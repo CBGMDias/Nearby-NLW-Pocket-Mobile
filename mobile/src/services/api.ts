cca7815f1c8875aa5fa1658572f9e9d7a134be06
Campolim <114692497+CBGMDias@users.noreply.github.com>
2024-12-13 18:03:04 -0300

    Commit aula 3

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://192.168.15.10:3333",
-    timeout: 700
+  baseURL: "http://192.168.15.200:3333",
+  timeout: 700,
 })
